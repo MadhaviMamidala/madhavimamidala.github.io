79eafe722c094afe2d820dd06071eea03cb9b44a
MadhaviMamidala <madhu12mamidala@gmail.com>
2025-04-16 02:01:53 -0700

    Add React application files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Madhavi Mamidala</h1>
+        <p>Welcome to my portfolio website</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
